ce54b12a56ba92e2a5fd5683cddf990e6dcaff36
Lartey Joshua <larteyjoshua@gmail.com>
2019-09-20 22:08:49 +0000

    PipFile updatd

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def edit_question():
 
 if __name__ == '__main__':
      port=os.environ.get('PORT', 5000)
-    app.run(debug=True, host='0.0.0.0', port=port)
+     app.run(debug=True, host='0.0.0.0', port=port)
